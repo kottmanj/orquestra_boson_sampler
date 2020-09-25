d3a8947af75efc1a39a26fea281a5881ef373600
Jakob S Kottmann <jakob.kottmann@gmail.com>
2020-09-25 17:02:38 -0400

    update

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="boson_sampler",
+    version="XXX",
+    py_modules=['bosonsampler'],
+    install_requires= ['photonic @ git+https://github.com/kottmanj/Photonic.git' ]
+)
+
